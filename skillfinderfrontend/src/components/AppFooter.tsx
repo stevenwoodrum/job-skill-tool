b6008fadec1004b997ee78ccb767ba9554d18ef0
Steven Woodrum <stevenwoodrum@users.noreply.github.com>
2026-05-10 20:38:35 -0400

    Add disclaimer to app footer

--- skillfinderfrontend/src/components/AppFooter.tsx
+++ skillfinderfrontend/src/components/AppFooter.tsx
@@ -6,9 +6,8 @@ function AppFooter() {
     return (
         <footer className="container">
             <section className="copyright">
-                ©2026 SkillLens
-            </section>
-            <section className="links">
+                ©2026 SkillLens. By entering data into this website, you consent to it being shared with third party service providers, each with their own terms of service. It is recommended that you anonymize your data before submission.
+
             </section>
         </footer>
     )
